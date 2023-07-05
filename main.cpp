92dc484151210dbb90c465af68388354aba90e62
alireza00249 <alirezasalati1381@gmail.com>
2023-07-05 21:06:23 +0330

    my first pull

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+int factor(int);
+float neper(float);
+int main()
+{
+    int N;
+    cout<<"Enter your number of sentenc: ";
+    cin>>N;
+    cout<<neper(N)<<"\n";
+    return 0;
+}
+int factor(int f){
+    int x=f-1,y=0;
+    when(f!=1){
+        f*=x;
+        x--;
+    }
+    return f;
+}
+float neper(float n){
+    float ne=0;
+    for(int i=1;i<=n;i++){
+        ne+=1.0/factor(i);
+    }
+    return ne;
+}
